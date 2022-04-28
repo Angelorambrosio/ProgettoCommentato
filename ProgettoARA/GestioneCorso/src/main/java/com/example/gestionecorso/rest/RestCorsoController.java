dee26d51c9c5b8cf83f747a90d48d0b6e72e6c9d
AntonioMug <102025264+AntonioMug@users.noreply.github.com>
2022-04-28 12:58:03 +0200

    Update RestCorsoController.java

--- ProgettoARA/GestioneCorso/src/main/java/com/example/gestionecorso/rest/RestCorsoController.java
+++ ProgettoARA/GestioneCorso/src/main/java/com/example/gestionecorso/rest/RestCorsoController.java
@@ -30,6 +30,20 @@ public class RestCorsoController {
 
 
     /*Non dovremmo controllare se il corso esiste prima di modificarlo?*/
+    /* ANTONIO
+        Teoricamente si sarebbe piu corretto.
+        Anzi in realtà sarebbe più corretto fare in questo modo: 
+        se guardi la riga 26 e la riga 40 praticamente stiamo definendo due end-point 
+        che fanno esattamente la stessa cosa ossiamo corsoService.save(corso);
+        sarebbe più corretto creare un solo end-point magari con il nome SalvaCorso
+        e utilizzare quella funzione. Hibernate e soprattutto la libreria JPARepository
+        riesce ad identificare se è un nuovo Corso ( in questo caso ) oppure se è uno da
+        aggiornare. Praticamente se gli passi un nuovo corso con id: 0 , lui lo aggiungerà 
+        alla tabella, oppure se gli passi un id che non esiste sulla tabella Corso nel database
+        lui farà un nuovo inserimento. Se gli passi un Corso con un id presente sul database
+        lui provvederà ad aggiornare quello già presente
+    */
+    
     @PostMapping(value = "/modificaCorso")
     public void modificaCorso(@RequestBody Corso corso){corsoService.save(corso);}
 
