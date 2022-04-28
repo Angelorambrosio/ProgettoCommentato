90fa5a8797f27b7bbb6d0a3c164ac413c4b7a241
AntonioMug <102025264+AntonioMug@users.noreply.github.com>
2022-04-28 12:52:28 +0200

    Update CorsoDTO.java

--- ProgettoARA/GestioneCorso/src/main/java/com/example/gestionecorso/data/dto/CorsoDTO.java
+++ ProgettoARA/GestioneCorso/src/main/java/com/example/gestionecorso/data/dto/CorsoDTO.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import java.time.LocalDate;
 
 /*In questa classe specifichiamo gli attributi che vogliamo far visualizzare
-* all'utente.*/
+* all'utente ( al client chiamante è la definizione più corretta, perché il chiamante potrebbe essere un 
+  altro microservizio o un front end , non per forza direttamente l'utente.*/
 @Data
 public class CorsoDTO {
 
